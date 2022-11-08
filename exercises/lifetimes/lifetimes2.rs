7a1cac352cad1f80787155a58ed495e9804938a4
Mayflyyh <mayflyyh@LAPTOP-LRPM608U.localdomain>
2022-11-08 21:36:45 +0800

    update

--- exercises/lifetimes/lifetimes2.rs
+++ exercises/lifetimes/lifetimes2.rs
@@ -8,7 +8,7 @@
 
 // I AM NOT DONE
 
-fn longest<'a>(x: &'a str, y: &'a str) -> &'a str {
+fn longest<'a,'b>(x: &'a str, y: &'a str) -> &'a str {
     if x.len() > y.len() {
         x
     } else {
